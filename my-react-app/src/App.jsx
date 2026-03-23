8ee1cd260d500cec78f95c3cba0ba0eafb7932bd
Shashank upadhyay <Shashanku943@gmail.com>
2026-03-24 01:00:56 +0530

    Made Card Component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,16 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import Food from "./Food";
+import Card from "./Card";
 
 function App() {
   return(
     <>
-        <Header />
-        <Food/>
-        <Footer  />
+        {/* <Header /> */}
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        { /* <Footer  /> */}
     </>
 
   )
